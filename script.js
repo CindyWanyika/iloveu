c62d2fec1544f183ef06f08d309199bd1287d567
cindy-kilonzo <cindy.kilonzo@ashesi.edu.gh>
2025-04-28 14:18:57 +0000

    update

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
-const sr = ScrollReveal({ distance: "50px", origin: "left", duration: 1500 });
+const sr = ScrollReveal({ distance: "50px", origin: "left", duration: 500 });
 
 const srTop = ScrollReveal({ distance: "50px", origin: "top", duration: 500 });
 
 const srBottom = ScrollReveal({ distance: "50px", origin: "bottom", duration: 5500 });
 
-const srRight = ScrollReveal({ distance: "50px", origin: "right", duration: 1500 });
+const srRight = ScrollReveal({ distance: "50px", origin: "right", duration: 500 });
 
 sr.reveal(".home-content h1", { delay: 500 });
 sr.reveal(".home-content p", { delay: 700 });
@@ -32,7 +32,7 @@ function launchConfetti() {
     })();
 }
 
-srRight.reveal(".v2", { delay: 1000 });
+srRight.reveal(".v2", { delay: 700 });
 
 
 srBottom.reveal(".horizontal-scroll-wrapper",{delay: 1000})
